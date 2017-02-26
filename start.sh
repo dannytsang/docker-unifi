9857655a6ad70d0646c4d8fe07b74091ec94d4db
Danny Tsang <danny@dannytsang.co.uk>
2017-02-26 16:23:09 +0000

    Added timezone to environment variable.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run --restart=always --net=host -d -v ~/contents/unifi/data:/var/lib/unifi:rw -v /var/log:/var/log --name unifi jacobalberty/unifi:latest 
+sudo docker run --restart=always --net=host -d -e TZ="Europe/London" -v ~/contents/unifi/data:/var/lib/unifi:rw -v /var/log:/var/log --name unifi jacobalberty/unifi:latest 
 sudo docker ps
