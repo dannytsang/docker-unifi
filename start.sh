e1403a0bf21ff4ab9852b6a61ad83c87b116ae01
Danny Tsang <danny@dannytsang.co.uk>
2018-03-14 22:42:13 +0000

    Updated files to new volume structure.
    Added running as non root parameter.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run --restart=always --net=host -d -e TZ="Europe/London" -v ~/contents/unifi/data:/var/lib/unifi:rw -v /var/log:/var/log --name unifi jacobalberty/unifi:latest 
+sudo docker run --restart=always --net=host -d -e TZ="Europe/London" -e RUNAS_UID0="false" -e UNIFI_UID="1000" -e UNIFI_GID="1000" -v ~/contents/unifi/data:/unifi/data:rw -v /var/log:/unifi/log --name unifi jacobalberty/unifi:latest 
 sudo docker ps
