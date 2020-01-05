d4782123f8732a9aa03f2566c8ad5e5c8254f0fc
Danny Tsang <danny@dannytsang.co.uk>
2020-01-05 15:07:55 +0000

    Fixed cron update script not working with relative pathing and updating docker pull / rm.

--- cron.sh
+++ cron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo /usr/local/bin/docker-compose pull
-sudo /usr/local/bin/docker-compose down -v
-sudo /usr/local/bin/docker-compose up -d
+sudo /usr/local/bin/docker-compose -f $PWD/docker-compose.yml stop
+sudo /usr/local/bin/docker-compose -f $PWD/docker-compose.yml rm -f
+sudo /usr/local/bin/docker-compose -f $PWD/docker-compose.yml pull
+sudo /usr/local/bin/docker-compose -f $PWD/docker-compose.yml up -d
